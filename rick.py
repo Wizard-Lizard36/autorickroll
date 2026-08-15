4e73ac8ab0f5073b24f435275212373b2cf5824c
Wizard-Lizard36 <max@bartle.co.nz>
2026-08-15 14:11:41 +1200

    Remove computer specific instruction

--- rick.py
+++ rick.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import webbrowser
-sys.path.insert(0, r"C:\Users\max\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages")
 import keyboard
 import time
 
